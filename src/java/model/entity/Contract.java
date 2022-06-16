4445fdbdf1084bea19ad6f2ac3a37781fcd634d4
ThanhNhan <hotanthanhnhan@gmail.com>
2022-06-16 22:09:49 +0700

    update name empName

--- src/java/model/entity/Contract.java
+++ src/java/model/entity/Contract.java
@@ -18,16 +18,16 @@ public class Contract {
     private Date tDate;
     private float salaryBasic;
     private String note;
-    private String empNameString;   
+    private String empName;   
 
-    public Contract(int id, int empID, Date fDate, Date tDate, float salaryBasic, String note, String empNameString) {
+    public Contract(int id, int empID, Date fDate, Date tDate, float salaryBasic, String note, String empName) {
         this.id = id;
         this.empID = empID;
         this.fDate = fDate;
         this.tDate = tDate;
         this.salaryBasic = salaryBasic;
         this.note = note;
-        this.empNameString = empNameString;
+        this.empName = empName;
     }
 
     public int getId() {
@@ -78,17 +78,17 @@ public class Contract {
         this.note = note;
     }
 
-    public String getEmpNameString() {
-        return empNameString;
+    public String getEmpName() {
+        return empName;
     }
 
-    public void setEmpNameString(String empNameString) {
-        this.empNameString = empNameString;
+    public void setEmpName(String empName) {
+        this.empName = empName;
     }
 
     @Override
     public String toString() {
-        return "Contract{" + "id=" + id + ", empID=" + empID + ", fDate=" + fDate + ", tDate=" + tDate + ", salaryBasic=" + salaryBasic + ", note=" + note + ", empNameString=" + empNameString + '}';
+        return "Contract{" + "id=" + id + ", empID=" + empID + ", fDate=" + fDate + ", tDate=" + tDate + ", salaryBasic=" + salaryBasic + ", note=" + note + ", empName=" + empName + '}';
     }
    
     
